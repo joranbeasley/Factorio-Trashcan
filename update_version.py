60381eadc003eebd393b915043e873f43ed66758
joran <joran@decagon.com>
2016-04-22 22:30:06 -0700

    working on the deploy step

--- update_version.py
+++ update_version.py
@@ -3,7 +3,7 @@ import os
 import shutil
 import tempfile
 from deploy_utils import find_package_folder,zipdir,get_version_tag
-
+import zipfile
 tag = get_version_tag()
 package_directory = find_package_folder()
 info_file = os.path.join(package_directory,"info.json")
@@ -15,6 +15,6 @@ package_name = os.path.basename(package_directory)+"_"+tag
 tmp_dir = tempfile.mkdtemp()
 dst_dir = os.path.join(tmp_dir,package_name)
 shutil.copytree(package_directory,dst_dir)
-zipdir(dst_dir,open(package_name+".zip","wb"))
+zipdir(dst_dir,zipfile.ZipFile(package_name+".zip","w"))
 shutil.rmtree(tmp_dir)
 print package_name+".zip"
